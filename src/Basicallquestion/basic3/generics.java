4a8d061d0f3a5010b8e8d8c298f6613ea160ea8c
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2022-07-01 22:46:10 +0530

    program

---
+++ src/Basicallquestion/basic3/generics.java
@@ -0,0 +1,17 @@
+package Basicallquestion.basic3;
+
+
+
+
+public class generics{
+    public static <T1,T2,T3> void temp(T1 x, T2 y,T3 z)
+    {
+        System.out.println("This is x="+x);
+        System.out.println("This is y="+y);
+        System.out.println("This is z="+z);
+    }
+    public static void main(String args[])
+    {
+         temp(1,2,3);
+    }
+}
